65260d19f7cfad5b9fde6321a5c5261be5f26aae
tianlu <tl965246161@163.com>
2021-11-13 16:08:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author tianlu
+ * @date 2021/11/13 0013 16:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
